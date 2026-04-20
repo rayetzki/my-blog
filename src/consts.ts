c3ea69249d625a0a9d82cf57dcf01776b854965f
Illia Raietskyi <illia.raietskyi.ext@vay.io>
2026-04-20 16:12:48 +0300

    Add favicons

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Блог Іллі Раєцького';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Ukraina Libertaria';
+export const SITE_DESCRIPTION = 'Персональний блог Іллі Раєцького';
